e6a74c7c91b946f95d6342c67ad04b2113610c29
Duncan Murphy <97642890+WiDuMu@users.noreply.github.com>
2025-02-11 12:08:17 -0600

    static db for example

--- docs/network.js
+++ docs/network.js
@@ -1,8 +1,8 @@
-const urlDefault = `http://localhost:8080`;
+// const urlDefault = `http://localhost:8080`;
 
 export async function getLocations(url) {
-   url = (url) ? url : urlDefault;
-   const request = fetch(`${url}/api/locations`);
+   // url = (url) ? url : urlDefault;
+   const request = fetch(`${url}/database.json`);
    const reponse = await request;
    const json = await reponse.json();
    return json;
